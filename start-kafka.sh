41d7357f929b3dde9beadc845ba0050d00d8b41c
xyjwsj <709283517@qq.com>
2017-07-06 10:53:15 +0800

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -3,5 +3,7 @@
 echo "${BROKER_ID}" >> $KAFKA_HOME/config/server.properties
 echo "${LISTENER_ADDR}" >> $KAFKA_HOME/config/server.properties
 echo "${ZOOKEEPER_CONNECT}" >> $KAFKA_HOME/config/server.properties
+#advertised.listeners=PLAINTEXT://192.168.150.158:9092
+echo "${ADVERTISED_LISTENER}" >> $KAFKA_HOME/config/server.properties
 
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
