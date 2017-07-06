ff4bce28f84d4fd0879d30bd73da4042ed8ca105
xyjwsj <709283517@qq.com>
2017-07-06 10:41:30 +0800

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,3 +1,6 @@
 
+echo "${BROKER_ID}" >> $KAFKA_HOME/config/server.properties
+echo "${LISTENER_ADDR}" >> $KAFKA_HOME/config/server.properties
+echo "${ZOOKEEPER_CONNECT}" >> $KAFKA_HOME/config/server.properties
 
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
