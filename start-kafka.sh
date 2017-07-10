05f68dd0262f1476f762415e93519031f3d0aa0a
xyjwsj <709283517@qq.com>
2017-07-10 14:12:34 +0800

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -6,4 +6,4 @@ echo "${ZOOKEEPER_CONNECT}" >> $KAFKA_HOME/config/server.properties
 #advertised.listeners=PLAINTEXT://192.168.150.158:9092
 echo "${ADVERTISED_LISTENER}" >> $KAFKA_HOME/config/server.properties
 
-exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties start-foreground
