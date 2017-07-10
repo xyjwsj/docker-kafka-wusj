9169b3078efb113007eea1ee8d9054eed2903c03
xyjwsj <709283517@qq.com>
2017-07-10 15:14:22 +0800

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-echo "broker.id=${BROKER_ID}" >> $KAFKA_HOME/config/server.properties
-echo "listeners=${LISTENER_ADDR}" >> $KAFKA_HOME/config/server.properties
-echo "zookeeper.connect=${ZOOKEEPER_CONNECT}" >> $KAFKA_HOME/config/server.properties
+KAFKA_CFG="$KAFKA_HOME/config/server.properties"
+
+echo "broker.id=${BROKER_ID}" >> ${KAFKA_CFG}
+echo "listeners=${LISTENER_ADDR}" >> ${KAFKA_CFG}
+echo "zookeeper.connect=${ZOOKEEPER_CONNECT}" >> ${KAFKA_CFG}
 #advertised.listeners=PLAINTEXT://192.168.150.158:9092
-echo "advertised.listeners=${ADVERTISED_LISTENER}" >> $KAFKA_HOME/config/server.properties
+echo "advertised.listeners=${ADVERTISED_LISTENER}" >> ${KAFKA_CFG}
 
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties start-foreground
