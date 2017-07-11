6bb90dd81b128d4f2769c1badcf91c48e079c664
xyjwsj <709283517@qq.com>
2017-07-11 11:18:32 +0800

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -3,8 +3,11 @@
 KAFKA_CFG="$KAFKA_HOME/config/server.properties"
 
 echo "broker.id=${BROKER_ID}" >> ${KAFKA_CFG}
-echo "listeners=PLAINTEXT://${LISTENER_ADDR}" >> ${KAFKA_CFG}
-echo "advertised.listeners=PLAINTEXT://${ADVERTISED_LISTENER}" >> ${KAFKA_CFG}
+# echo "listeners=PLAINTEXT://${LISTENER_ADDR}" >> ${KAFKA_CFG}
+# echo "advertised.listeners=PLAINTEXT://${ADVERTISED_LISTENER}" >> ${KAFKA_CFG}
+echo "advertised.host.name=${ADVERTISED_HOST}" >> ${KAFKA_CFG}
+echo "advertised.port=${ADVERTISED_PORT}" >> ${KAFKA_CFG}
+
 echo "zookeeper.connect=${ZOOKEEPER_CONNECT}" >> ${KAFKA_CFG}
 
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
