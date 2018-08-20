98018fa44554672eb338a073d8936f311b48481f
xyjwsj <709283517@qq.com>
2018-08-20 16:02:51 +0800

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -4,7 +4,7 @@ KAFKA_CFG="$KAFKA_HOME/config/server.properties"
 
 echo "broker.id=${BROKER_ID}" >> ${KAFKA_CFG}
 # echo "listeners=PLAINTEXT://${LISTENER_ADDR}" >> ${KAFKA_CFG}
-echo "advertised.listeners=PLAINTEXT://${ADVERTISED_HOST}:${ADVERTISED_PORT}" >> ${KAFKA_CFG}
+# echo "advertised.listeners=PLAINTEXT://${ADVERTISED_HOST}:${ADVERTISED_PORT}" >> ${KAFKA_CFG}
 echo "advertised.host.name=${ADVERTISED_HOST}" >> ${KAFKA_CFG}
 echo "advertised.port=${ADVERTISED_PORT}" >> ${KAFKA_CFG}
 
